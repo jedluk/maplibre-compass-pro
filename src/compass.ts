0e157d3b87e594dff47b5fbc69d403a405271bf9
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-03-19 16:57:21 +0100

    Adjust map-gl-compass-pro to standalone maplibre version

---
+++ src/compass.ts
@@ -0,0 +1,93 @@
+import './compass.css'
+import { IControl, Map } from 'maplibre-gl'
+
+interface CompassProps {
+    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+    visualizePitch?: boolean
+    onClick?: () => void
+}
+
+export class Compass implements IControl {
+    _map: Map
+    size: NonNullable<CompassProps['size']>
+    visualizePitch: boolean
+    compassElement: HTMLDivElement
+    customClick?: () => void
+
+    constructor({ size = 'md', visualizePitch = true, onClick }: CompassProps) {
+        this.size = size
+        this.visualizePitch = visualizePitch
+        this.customClick = onClick
+    }
+
+    onAdd(map: Map): HTMLElement {
+        this._map = map
+        map.on('rotate', this.handleMapJog)
+        map.on('pitch', this.handleMapJog)
+        return this.createCompassElement()
+    }
+
+    onRemove(map: Map): void {
+        map.off('rotate', this.handleMapJog)
+        map.off('pitch', this.handleMapJog)
+    }
+
+    getDefaultPosition() {
+        return 'bottom-left' as const
+    }
+
+    changeSize = (size: NonNullable<CompassProps['size']>) => {
+        this.size = size
+        this.compassElement.setAttribute('data-size', this.size)
+    }
+
+    handleClick = () => {
+        if (this.customClick) {
+            this.customClick()
+        } else {
+            this._map.resetNorth()
+            this._map.resetNorthPitch()
+        }
+    }
+
+    handleMapJog = () => {
+        const bearing = -1 * this._map.getBearing()
+        let transform = `rotate(${bearing}deg)`
+        if (this.visualizePitch) {
+            const pitch = this._map.getPitch()
+            transform += ` rotateX(${pitch}deg)`
+        }
+        this.compassElement.style.transform = transform
+    }
+
+    createCompassElement = (): HTMLElement => {
+        const container = document.createElement('div')
+        container.classList.add('compass-pro-wrapper')
+        container.setAttribute('data-size', this.size)
+
+        const compass = document.createElement('div')
+        compass.classList.add('compass-pro')
+        compass.setAttribute('data-size', this.size)
+        compass.addEventListener('click', this.handleClick)
+
+        const needles: HTMLElement[] = []
+
+        for (let i = 1; i <= 6; i++) {
+            const needle = document.createElement('div')
+            needle.classList.add('needle')
+            needles.push(needle)
+        }
+
+        const innerFace = document.createElement('div')
+        innerFace.classList.add('inner-face')
+
+        const needleNorth = document.createElement('div')
+        needleNorth.classList.add('needlde-north')
+
+        compass.append(...needles, needleNorth, innerFace)
+        this.compassElement = compass
+        container.append(compass)
+
+        return container
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './style.css'
+import maplibregl from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import { Compass } from './compass';
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: '/mapStyle.json',
+  center: [14.5, 53.4],
+  zoom: 11
+});
+
+
+let idx = 0
+const sizes = ['xs', 'sm', 'md', 'lg', 'xl'] as const
+
+const compass = new Compass({ size: 'md' })
+map.addControl(compass)
+
+// setInterval(() => {
+// compass.changeSize(sizes[idx++ % sizes.length])
+// }, 1_000)
