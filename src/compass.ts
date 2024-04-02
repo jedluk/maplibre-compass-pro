13186f6b344e035bc8551fda204d3483fb582f14
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-04-02 18:24:13 +0200

    PoC

--- src/compass.ts
+++ src/compass.ts
@@ -3,6 +3,7 @@ import { type IControl, type Map } from 'maplibre-gl'
 
 export type CompassProps = {
 	size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+	literals?: boolean
 	visualizePitch?: boolean
 	onClick?: () => void
 }
@@ -11,16 +12,20 @@ export class Compass implements IControl {
 	_map?: Map
 	size: NonNullable<CompassProps['size']>
 	visualizePitch: boolean
+	literals: boolean
 	compassElement?: HTMLDivElement
+	literalElement?: HTMLImageElement
 	customClick?: () => void
 
 	constructor({
 		size = 'md',
 		visualizePitch = false,
+		literals = false,
 		onClick,
 	}: CompassProps = {}) {
 		this.size = size
 		this.visualizePitch = visualizePitch
+		this.literals = literals
 		this.customClick = onClick
 	}
 
@@ -58,11 +63,27 @@ export class Compass implements IControl {
 		if (!this._map || !this.compassElement) {
 			return
 		}
+
 		const bearing = -1 * this._map.getBearing()
 		let transform = `rotate(${bearing}deg)`
 		if (this.visualizePitch) {
 			const pitch = this._map.getPitch()
 			transform += ` rotateX(${pitch}deg)`
+		}
+		if (this.literals && this.literalElement) {
+			if (bearing > -135 && bearing < -45) {
+				this.literalElement.src = '/east.svg'
+				this.literalElement.style.transform = 'rotate(90deg)'
+			} else if (bearing >= -45 && bearing < 45) {
+				this.literalElement.src = '/north.svg'
+				this.literalElement.style.transform = ''
+			} else if (bearing >= 45 && bearing < 135) {
+				this.literalElement.src = '/west.svg'
+				this.literalElement.style.transform = 'rotate(-90deg)'
+			} else {
+				this.literalElement.src = '/south.svg'
+				this.literalElement.style.transform = 'rotate(180deg)'
+			}
 		}
 		this.compassElement.style.transform = transform
 	}
@@ -78,21 +99,30 @@ export class Compass implements IControl {
 		compass.setAttribute('data-size', this.size)
 		compass.addEventListener('click', this.handleClick)
 
-		const needles: HTMLElement[] = []
+		const children: HTMLElement[] = []
 
 		for (let i = 1; i <= 6; i++) {
 			const needle = document.createElement('div')
 			needle.classList.add('needle')
-			needles.push(needle)
+			children.push(needle)
 		}
 
 		const innerFace = document.createElement('div')
 		innerFace.classList.add('inner-face')
+		children.push(innerFace)
 
-		const needleNorth = document.createElement('div')
-		needleNorth.classList.add('needlde-north')
+		if (this.literals) {
+			const img = document.createElement('img')
+			img.src = '/north.svg'
+			this.literalElement = img
+			innerFace.appendChild(img)
+		} else {
+			const needleNorth = document.createElement('div')
+			needleNorth.classList.add('needlde-north')
+			children.push(needleNorth)
+		}
 
-		compass.append(...needles, needleNorth, innerFace)
+		compass.append(...children)
 		this.compassElement = compass
 		container.append(compass)
 
