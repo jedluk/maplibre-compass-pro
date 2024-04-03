3b67dc2029362bc3bd24ba5bd4b41058f7104c9d
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-04-03 16:04:50 +0200

    update compass API

--- src/lib.ts
+++ src/lib.ts
@@ -1,13 +1,13 @@
-import { DIRECTION_ICONS } from "./icons"
+import { DIRECTION_ICONS } from './icons'
 
 export function mapBearingToIcon(bearing: number) {
-    if (bearing > -135 && bearing < -45) {
-        return DIRECTION_ICONS.east
-    } else if (bearing >= -45 && bearing < 45) {
-        return DIRECTION_ICONS.north
-    } else if (bearing >= 45 && bearing < 135) {
-        return DIRECTION_ICONS.west
-    } else {
-        return DIRECTION_ICONS.south
-    }
+	if (bearing > -135 && bearing < -45) {
+		return DIRECTION_ICONS.east
+	} else if (bearing >= -45 && bearing < 45) {
+		return DIRECTION_ICONS.north
+	} else if (bearing >= 45 && bearing < 135) {
+		return DIRECTION_ICONS.west
+	} else {
+		return DIRECTION_ICONS.south
+	}
 }
