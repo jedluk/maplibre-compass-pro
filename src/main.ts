e83cb2a6518c8d222d1fcf695125dd2c8f46207d
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-04-04 09:02:16 +0200

    Update styles

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ const map = new maplibregl.Map({
 const sizes = ['xs', 'sm', 'md', 'lg', 'xl'] as const
 
 map.addControl(
-	new Compass({ size: 'md', visualizePitch: false, displayDirection: true }),
+	new Compass({ size: sizes[2], visualizePitch: false, displayDirection: true }),
 	'bottom-left',
 )
