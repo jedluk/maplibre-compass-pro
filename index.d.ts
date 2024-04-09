da539ec27a10e37fa6c19cea1135edcb7567f929
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-04-09 14:30:19 +0200

    Private members & methods (#5)

--- index.d.ts
+++ index.d.ts
@@ -4,22 +4,13 @@ import { IControl } from 'maplibre-gl';
 import { Map as Map_2 } from 'maplibre-gl';
 
 export declare class Compass implements IControl {
-    _map?: Map_2;
-    size: NonNullable<CompassProps['size']>;
-    visualizePitch: boolean;
-    displayDirection: boolean;
-    compassElement?: HTMLDivElement;
-    customClick?: () => void;
+    #private;
     constructor({ size, visualizePitch, displayDirection, onClick, }?: CompassProps);
     onAdd(map: Map_2): HTMLElement;
     onRemove(map: Map_2): void;
     getDefaultPosition(): "bottom-left";
-    changeSize: (size: NonNullable<CompassProps['size']>) => void;
-    handleClick: () => void;
-    handleMapJog: () => void;
-    toggle: () => void;
-    createNeedle(): HTMLDivElement;
-    createCompassElement: () => HTMLElement;
+    changeSize(size: NonNullable<CompassProps['size']>): void;
+    toggle(): void;
 }
 
 export declare type CompassProps = {
