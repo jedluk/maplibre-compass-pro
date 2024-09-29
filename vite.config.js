8dad6402ce86e7153b5d97d1d73312b2f6322d59
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2024-09-29 22:25:08 +0200

    Move types into separated dir

--- vite.config.js
+++ vite.config.js
@@ -3,25 +3,26 @@ import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig(({ command }) => {
-  return {
-    publicDir: command === 'serve',
-    build: {
-      lib: {
-        entry: resolve(__dirname, 'src/compass.ts'),
-        name: 'compass',
-        fileName: 'compass',
-      },
-      rollupOptions: {
-        external: ['maplibre-gl'],
-      },
-    },
-    ...(command === 'build' && {
-      plugins: [
-        dts({
-          rollupTypes: true,
-          outDir: __dirname,
-        }),
-      ],
-    }),
-  }
+	return {
+		publicDir: command === 'serve',
+		build: {
+			lib: {
+				entry: resolve(__dirname, 'src/compass.ts'),
+				name: 'compass',
+				fileName: 'compass',
+			},
+			rollupOptions: {
+				external: ['maplibre-gl'],
+			},
+		},
+		...(command === 'build' && {
+			plugins: [
+				dts({
+					rollupTypes: true,
+					outDir: 'types',
+					insertTypesEntry: true,
+				}),
+			],
+		}),
+	}
 })
